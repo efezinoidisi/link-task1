f8a097094da56f173cdbe8756129088a02c0c296
Efezino Idisi <enduranceidisi13@gmail.com>
2022-10-26 15:04:04 +0100

    start project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<Router></Router>
+		</div>
+	);
+}
+
+export default App;
