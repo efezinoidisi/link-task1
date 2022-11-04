65b33eaa62408b067c230bc513ac41b3a6019507
Efezino Idisi <enduranceidisi13@gmail.com>
2022-11-04 07:28:18 +0100

    refactor: inputs data

---
+++ src/inputs.js
@@ -0,0 +1,30 @@
+const inputs = [
+	{
+		id: "first_name",
+		name: "first_name",
+		type: "text",
+		title: "form--input",
+		placeholder: "Enter your first name",
+		label: "First name",
+	},
+
+	{
+		id: "last_name",
+		name: "last_name",
+		type: "text",
+		title: "form--input",
+		placeholder: "Enter your last name",
+		label: "Last name",
+	},
+
+	{
+		id: "email",
+		name: "email",
+		type: "email",
+		title: "form--input",
+		placeholder: "yourname@email.com",
+		label: "Email",
+	},
+];
+
+export default inputs;
