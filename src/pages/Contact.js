0881ddeb2810a2a40b6e62ae11c108a0b55b13c6
Efezino Idisi <enduranceidisi13@gmail.com>
2022-11-05 03:07:26 +0100

    feat: message validation

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,5 +1,5 @@
 import Input from "../components/Input";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 //import inputs from "../inputs";
 import "./css/contact.css";
 
@@ -12,34 +12,42 @@ const Contact = () => {
 	});
 
 	const msg = "Send me a message and I'll reply you as soon as possible";
-	const name = "Idisi Efezino";
+	const name = "Idisi Efezino Endurance";
+	const [errorMessage, setErrorMessage] = useState("");
+	const [isValidated, setIsValidated] = useState(false);
+
+	const [isChecked, setIsChecked] = useState(false);
+
+	const checkHandler = () => {
+		setIsChecked(!isChecked);
+	};
 
 	const handleChange = (e) => {
 		setValues({ ...values, [e.target.name]: e.target.value });
-		console.log(values);
 	};
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
+		setErrorMessage(validate(values.message));
+		setIsValidated(true);
 	};
+
+	const validate = (msg) => {
+		const error = "Please enter a message";
+		if (!msg) return error;
+		else return "";
+	};
+	useEffect(() => {
+		if (errorMessage.length === 0 && isValidated) {
+		}
+	}, [errorMessage, isValidated, values]);
+
 	return (
 		<div className="contact--page">
 			<h2>Contact Me</h2>
 			<p>Hi there, contact me to ask me about anything you have in mind.</p>
 
 			<form onSubmit={handleSubmit}>
-				{/* {inputs.map((input) => (
-					<Input
-						key={input.id}
-						id={input.id}
-						title={input.title}
-						type={input.type}
-						placeholder={input.placeholder}
-						label={input.label}
-						value={values[input.name]}
-						handleChange={handleChange}
-					/>
-				))} */}
 				<div className="form--first-last">
 					<Input
 						type="text"
@@ -76,44 +84,40 @@ const Contact = () => {
 					placeholder="yourname@email.com"
 					label="Email"
 				/>
+				<div className="form--input-message">
+					<label htmlFor="message">Message</label>
+					<textarea
+						name="message"
+						id="message"
+						cols="30"
+						rows="5"
+						value={values.message}
+						onChange={(e) => handleChange(e)}
+						placeholder={msg}
+						style={errorMessage ? { border: "1px solid #F89687" } : {}}
+					></textarea>
+					<span>{errorMessage}</span>
+				</div>
 
-				<label htmlFor="message">Message</label>
-				<textarea
-					name="message"
-					id="message"
-					cols="30"
-					rows="5"
-					value={values.message}
-					onChange={(e) => handleChange(e)}
-					placeholder={msg}
-					required
-				></textarea>
-
-				{/* <div className="confirm form-check">
+				<div className="confirm">
 					<input
 						type="checkbox"
-						name="confirm"
-						id="confirm"
+						name="form--consent"
+						id="form--consent"
+						onChange={checkHandler}
+						checked={isChecked}
 						className="form-check-input"
 					/>
-					<label htmlFor="confirm" className="form-check-label">
+					<label htmlFor="form--consent" className="form-check-label">
 						You agree to providing your data to {name} who may contact you.
 					</label>
-				</div> */}
-
-				<Input
-					type="checkbox"
-					id="form--consent"
-					title="form-check-input"
-					label={
-						"You agree to providing your data to " +
-						name +
-						" who may contact you."
-					}
-					groupName="confirm"
-				/>
-				<button id="btn__submit">Send message</button>
+				</div>
+				<button type="submit" id="btn__submit">
+					Send message
+				</button>
 			</form>
+
+			<p>{isValidated && !errorMessage ? "Message sent!" : ""}</p>
 		</div>
 	);
 };
